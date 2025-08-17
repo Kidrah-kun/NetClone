edd1eed2a7855554f6b3e2aaa91ebc3f84a707ee
Hardik Hathwal <132740042+Kidrah-kun@users.noreply.github.com>
2025-08-17 09:20:07 +0530

    added and fixed the cards on homepage.

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import './TitleCards.css'
+import Cards_data from '../../assets/cards/Cards_data'
 function TitleCards() {
   return (
     <div className='title-cards'>
-      
+      <h2>Popular on Netflix</h2>
+      <div className="card-list">
+        {Cards_data.map((card,index)=>{
+          return <div className="card" key={index}>
+            <img src={card.image} alt="" />
+            <p>{card.name}</p>
+          </div>
+        })}
+      </div>
     </div>
   )
 }
