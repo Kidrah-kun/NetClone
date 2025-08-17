977071c9e16a29f3b459f684993f5d8830ab107c
Hardik Hathwal <132740042+Kidrah-kun@users.noreply.github.com>
2025-08-17 13:56:30 +0530

    handling the sign in signup component.

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,18 +1,23 @@
-import React from 'react'   
+import React,{useState} from 'react' 
+  
 import './Login.css'
 import logo from '../../assets/logo.png'
 
 function Login() {
+// State to manage the sign in/sign up state
+const [signState,setsignState] = useState('sign In');
+
   return (
     <div className='login'>
       <img src={logo} alt="" className='login-logo' />
       <div className="login-form">
-        <h1>Sign Up</h1>
+        <h1>{signState}</h1>
         <form>
-          <input type="text" placeholder='Your Name'/>
+          {/* Toggle between Sign In and Sign Up */}
+          {signState === 'Sign Up' ?  <input type="text" placeholder='Your Name'/>:<></>}
           <input type="email" placeholder='Email Address'/>
           <input type="password" placeholder='Password'/>
-          <button type='submit'>Sign Up</button>
+          <button type='submit'>{signState}</button>
           <div className="form-help">
             <div className="remember">
               <input type="checkbox"/>
@@ -22,8 +27,11 @@ function Login() {
           </div>
         </form>
         <div className="form-switch">
-          <p>New to Netflix? <span>Sign Up now</span></p>
-          <p>Already have an account? <span>Sign In now</span></p>
+          {/* Toggle between Sign In and Sign Up */}
+          {signState === 'Sign In' ?
+          <p>New to Netflix? <span onClick={()=>setsignState('Sign Up')}>Sign Up now</span></p> :
+          <p>Already have an account? <span onClick={()=>
+            setsignState('Sign In')}>Sign In now</span></p>}
         </div>
       </div>
     </div>
