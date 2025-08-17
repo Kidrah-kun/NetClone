2da955ad696112d60dfd604ae981c6e900c0e80b
Hardik Hathwal <132740042+Kidrah-kun@users.noreply.github.com>
2025-08-17 13:36:57 +0530

    made the front-end structure of the Login Page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,31 @@
 import React from 'react'   
 import './Login.css'
+import logo from '../../assets/logo.png'
+
 function Login() {
   return (
     <div className='login'>
-      
+      <img src={logo} alt="" className='login-logo' />
+      <div className="login-form">
+        <h1>Sign Up</h1>
+        <form>
+          <input type="text" placeholder='Your Name'/>
+          <input type="email" placeholder='Email Address'/>
+          <input type="password" placeholder='Password'/>
+          <button type='submit'>Sign Up</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox"/>
+              <label htmlFor="">Remember Me</label>
+            </div>
+            <p>Need Help?</p>
+          </div>
+        </form>
+        <div className="form-switch">
+          <p>New to Netflix? <span>Sign Up now</span></p>
+          <p>Already have an account? <span>Sign In now</span></p>
+        </div>
+      </div>
     </div>
   )
 }
